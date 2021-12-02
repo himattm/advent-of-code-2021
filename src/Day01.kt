6a3bcec5a74f8315186493002f9393de1fc9fd41
Matthew McKenna <mmckenna@squareup.com>
2021-12-02 00:51:52 -0500

    Completed Day01 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -41,6 +41,39 @@ To do this, count the number of times a depth measurement increases from the pre
 In this example, there are 7 measurements that are larger than the previous measurement.
 
 How many measurements are larger than the previous measurement?
+
+--- Part Two ---
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A
+200  A B
+208  A B C
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G
+269    F G H
+260      G H
+263        H
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
  */
 
 fun main() {
@@ -64,7 +97,31 @@ fun main() {
   }
 
   fun part2(input: List<String>): Int {
-    return input.size
+    var result = 0
+    var previousSum = input // We need the first sum to start with so we are not off by 1
+      .take(3) // Take the first 3
+      .map { it.toInt() } // Map to Int
+      .sum() // Add them all together
+
+    input
+      .map { it.toInt() }
+      .windowed(
+        size = 3,
+        step = 1,
+        partialWindows = false, // Partial windows are not allowed from the prompt.
+      )
+      .map { window -> window.sum() }
+      .forEach { currentSum ->
+        // Check if the currentSum is greater than the previousSum
+        // On the first run these are guaranteed to be equal so we will not increment.
+        if (currentSum > previousSum) {
+          result++
+        }
+
+        previousSum = currentSum // Keep track of the previous amount for comparison.
+      }
+
+    return result
   }
 
   val input = readInput("Day01")
